ecec5a9ef1d4da20338d3872c4d4a9e63adbf1de
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2024-04-07 00:54:16 +0600

    all commited

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full bg-slate-300 flex justify-center items-center">
+      <div className="text-5xl text-blue-900 font-bold">Next JS</div>
+    </div>
+  );
+}
